e2ee7fb5ab3779f0b7e1743b55f375816635e1ba
Jonathan L. Komar <jonathan.komar@uibk.ac.at>
2021-03-14 22:32:10 +0100

    Made /var/lib/mcs 777 because of some unknown permissions issue. Also set perms of mcs home.

--- mcs-server/docker/entrypoint.sh
+++ mcs-server/docker/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-/setpermissions /usr/share/mcs $(id -u) $(id -g)
+echo "==> Running as $(id -u):$(id -g)"
+stat /var/lib/mcs
+/setpermissions $(id -u) $(id -g) /usr/share/mcs
+/setpermissions $(id -u) $(id -g) /var/lib/mcs
+/setpermissions $(id -u) $(id -g) /var/log/mcs
+/setpermissions $(id -u) $(id -g) /srv/mcs
 /usr/share/mcs/bin/start-mcs server
--- mcs-server/docker/mcs_test_helper.sh
+++ mcs-server/docker/mcs_test_helper.sh
@@ -1,4 +1,4 @@
 IMG=URL:VERSION
 docker rm -f mcs-test
-docker run -d --name mcs-test -e "TZ=Europe/Zurich" $IMG
+docker run -d --name mcs-test --user $(id -u dkrmcs):$(id -g dkrmcs) -e "TZ=Europe/Zurich" $IMG
 docker exec -it mcs-test bash
