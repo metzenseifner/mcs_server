99e5c8a56fac175571dd9b9ddf725ae21450acff
Jonathan L. Komar <jonathan.komar@uibk.ac.at>
2021-07-09 10:01:27 +0200

    Updated default esb url

--- mcs-server/components/bookingrepo-impl/src/main/java/at/ac/uibk/mcsconnect/bookingrepo/impl/hidden/EsbClientDefaults.java
+++ mcs-server/components/bookingrepo-impl/src/main/java/at/ac/uibk/mcsconnect/bookingrepo/impl/hidden/EsbClientDefaults.java
@@ -5,5 +5,5 @@ import java.net.URI;
 public abstract class EsbClientDefaults {
     public static final String DEFAULT_ESB_USERNAME = "admin";
     public static final String DEFAULT_ESB_PASSWORD = "admin";
-    public static final URI DEFAULT_ESB_URL = URI.create("https://esb01.uibk.ac.at/cxf/vle-connect/sis");
+    public static final URI DEFAULT_ESB_URL = URI.create("https://esb.uibk.ac.at/cxf/vle-connect/sis");
 }
